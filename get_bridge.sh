13d1c1c1559f0f2d85590286ffc9e351fecc376b
Doug Anson <doug.anson@arm.com>
2020-02-25 18:35:32 -0600

    updates

--- get_bridge.sh
+++ get_bridge.sh
@@ -70,19 +70,11 @@ SCRIPT_OPTIONS="[iotf | iothub | awsiot | google | mqtt | mqtt-getstarted | samp
 # Environment Selection
 #
 if [ "$(uname)" = "Darwin" ]; then
-    if [ ! -h /usr/local/bin/docker-machine ]; then
-        # MacOS (toolkit docker installed (OLD))... default is to pin IP address to 192.168.99.100
-        IP="192.168.99.100"
-	BASE_IP=${IP}
-        echo "IP Address:" ${IP}
-        IP=${IP}:
-    else
-        # MacOS (native docker installed) - use localhost..."
-	IP="127.0.0.1"
-	BASE_IP=${IP}
-        echo "IP Address:" ${IP}
-        IP=${IP}:
-    fi
+    # MacOS (native docker installed) - use localhost..."
+    IP="127.0.0.1"
+    BASE_IP=${IP}
+    echo "IP Address:" ${IP}
+    IP=${IP}:
 elif [ "$(uname)" = "MINGW64_NT-10.0" ]; then
     # Windows - Must use the Docker Toolkit with the latest VirtualBox installed... pinned to 192.168.99.100 
     IP="192.168.99.100"
